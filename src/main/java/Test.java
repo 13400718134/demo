1e27123e6b9938d847ffa492baaa7717d299ddcd
liu <mt@lxh.com>
2021-05-13 16:36:34 +0800

    third commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,6 +3,6 @@ package main.java;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello Beijing!");
-
+        System.out.println("master");
     }
 }
