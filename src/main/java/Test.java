25da15c374e6bd9ac00e11a92ebbca3bfae50893
liu <mt@lxh.com>
2021-05-13 16:35:58 +0800

    second commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,6 +3,7 @@ package main.java;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello Beijing!");
+        System.out.println("hot-fix");
 
     }
 }
